92d6e689fd7a788e353e9f84dfc76616334220af
adentintime <arixbizz@gmail.com>
2018-08-22 03:34:24 +0000

    Create api

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+from flask import Flask
+from api.v1.views import app_views
+from os import getenv
+from models import storage
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(self):
+    storage.close()
+
+if __name__ == "__main__":
+    hosti = getenv("HBNB_API_HOST", "0.0.0.0")
+    porti = getenv("HBNB_API_PORT", 5000)
+    app.run(host=hosti, port=porti, threaded=True)
