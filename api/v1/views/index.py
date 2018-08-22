9387979807d666a77b53e0e9bd40892b48bcef7e
adentintime <arixbizz@gmail.com>
2018-08-22 19:22:27 +0000

    Update index, add stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,8 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+import models
+from models import storage
 
 
 @app_views.route('/status')
@@ -11,3 +13,14 @@ def status():
         Returns status in json format
     '''
     return jsonify(status="OK")
+
+
+@app_views.route('/stats')
+def stats():
+    '''
+        Returns status in json format
+    '''
+    dic = {}
+    for x in models.classes:
+        dic[x.lower()+'s'] = storage.count(x)
+    return jsonify(dic)
