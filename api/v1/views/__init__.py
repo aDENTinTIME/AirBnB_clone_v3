e4ad3384de6df9f6fdc39741a3caa1a17d37955b
adentintime <arixbizz@gmail.com>
2018-08-23 20:49:00 +0000

    Update init with imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,9 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *  # noqa
+from api.v1.views.states import *  # noqa
+from api.v1.views.city import *  # noqa
+from api.v1.views.amenity import *  # noqa
+from api.v1.views.user import *  # noqa
+from api.v1.views.place import *  # noqa
+from api.v1.views.reviews import *  # noqa
